f777eae3bbbd253158658f0c42f0f7bab92dfcab
akniputra <aknibroken212@gmail.com>
2022-03-18 08:54:36 +0700

    pertemuan 2

--- src/main/kotlin/1_Array.kt
+++ src/main/kotlin/1_Array.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
 
-//Array
+//Array adalah koleksi data yang sejenis untuk membuat array sebuah array
     var arrA = arrayOf<Int>()
     arrA += 34
     println(arrA[0])
