8c5ef0a56ad9be48ec6f00df39bb7dc5b56ea3c3
akniputra <aknibroken212@gmail.com>
2022-03-18 14:39:59 +0700

    map

--- src/main/kotlin/12_Map.kt
+++ src/main/kotlin/12_Map.kt
@@ -9,7 +9,7 @@ keys are unique, but different keys can be paired with equal values.
     println("All values: ${numbersMap.values}")
     if ("key2" in numbersMap) println("Value by key \"key2\": ${numbersMap["key2"]}")
     if (1 in numbersMap.values) println("The value 1 is in the map")
-    if (numbersMap.containsValue(1)) println("The value 1 is in the map") // same as previous
+    if (numbersMap.containsValue(1)) println("The value 1 is in the map")
 //wo maps containing the equal pairs are equal regardless of the pair order.
     val anotherMap = mapOf("key2" to 2, "key1" to 1, "key4" to 1, "key3" to 3)
     println("The maps are equal: ${numbersMap == anotherMap}")
